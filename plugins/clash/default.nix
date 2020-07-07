b63d625720d113c33d12137b6516e93131b610f9
Harry Ying <lexugeyky@outlook.com>
2020-07-07 17:39:06 +0800

    Remove clash overlay

--- plugins/clash/default.nix
+++ plugins/clash/default.nix
@@ -60,7 +60,6 @@ in {
       (self: super: {
         maxmind-geoip = (super.callPackage ./packages/maxmind-geoip.nix { });
         yacd = (super.callPackage ./packages/yacd.nix { });
-        clash = (super.callPackage ./packages/clash.nix { });
       })
     ];
 
