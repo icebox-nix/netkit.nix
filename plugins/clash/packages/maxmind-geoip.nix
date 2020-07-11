fc542000bfc47f8305a2ef183bf885f56b74eac6
Harry Ying <lexugeyky@outlook.com>
2020-07-11 22:34:21 +0800

    Update GEOIP

--- plugins/clash/packages/maxmind-geoip.nix
+++ plugins/clash/packages/maxmind-geoip.nix
@@ -2,12 +2,12 @@
 
 stdenv.mkDerivation rec {
   pname = "maxmind-geoip";
-  version = "20200610";
+  version = "20200711";
 
   src = fetchurl {
     url =
       "https://github.com/Dreamacro/${pname}/releases/download/${version}/Country.mmdb";
-    sha256 = "1pfrnb1l9mnfi0crlbzixl4xzv17cr7gslba0plvfdzkk91ycxcg";
+    sha256 = "0sscd5gqr843k7lxk869d5imb2078i0qc3ssk6wz9n40gqg9nkb7";
   };
 
   phases = [ "installPhase" ];
