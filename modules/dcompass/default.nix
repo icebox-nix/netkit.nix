ecdfbb48442fdfd0c3ae765e8aba8fd6dd57702a
Harry Ying <lexugeyky@outlook.com>
2020-11-03 16:59:23 +0800

    Remove excessive overture in dcompass module

--- modules/dcompass/default.nix
+++ modules/dcompass/default.nix
@@ -4,11 +4,11 @@ self:
 with lib;
 
 let
-  cfg = config.netkit.overture;
+  cfg = config.netkit.dcompass;
   confFile =
     pkgs.writeText "dcompass-config.json" (generators.toJSON { } cfg.settings);
 in {
-  options.netkit.overture = {
+  options.netkit.dcompass = {
     enable = mkEnableOption "Dcompass DNS server";
 
     settings = mkOption {
@@ -27,7 +27,7 @@ in {
       isSystemUser = true;
     };
 
-    systemd.services.overture = {
+    systemd.services.dcompass = {
       description = "Dcompass DNS service";
       after = [ "network.target" ];
       wantedBy = [ "multi-user.target" ];
