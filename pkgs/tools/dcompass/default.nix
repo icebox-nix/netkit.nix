b5a8897a4f535424e4c6fd9e739f42f7b855fc32
Harry Ying <lexugeyky@outlook.com>
2020-11-10 08:38:05 +0800

    Upgrade dcompass

--- pkgs/tools/dcompass/default.nix
+++ pkgs/tools/dcompass/default.nix
@@ -7,9 +7,9 @@ rustPlatform.buildRustPackage rec {
   src = fetchFromGitHub {
     owner = "LEXUGE";
     repo = pname;
-    rev = "4da162b756ce11b88ea49ec640cd1e47fc1f282c";
-    sha256 = "0m59c88j8d3g2fn5xq6h8z6rhr0ls7c0k4my7c536sa0dnzvh7ax";
+    rev = "fb64f057493b723059d5e695e4d92cc694a7368b";
+    sha256 = "1lwsfbpc4ic2d0lrqarxybhygbyjn78m57xhazbwj44c9kxas4s9";
   };
 
-  cargoSha256 = "0vgb1vnbyzpl8ahcis4k2l7zpc8q7ydikpgdaqnkcbl553fshxv0";
+  cargoSha256 = "0bqpzxvr70aqhkyy0ag0wjc10jzzdiyrhgv2n87i8z0v4gzyq3jj";
 }
