0f78c34d0574b6256b27cf3b675eeea9213a745a
Harry Ying <lexugeyky@outlook.com>
2021-02-23 11:38:10 +0800

    update yacd

--- pkgs/tools/yacd/default.nix
+++ pkgs/tools/yacd/default.nix
@@ -1,38 +1,23 @@
-{ lib, yarn2nix-moretea, fetchFromGitHub }:
+{ lib, fetchzip, stdenv }:
 
-yarn2nix-moretea.mkYarnPackage rec {
-  name = "yacd";
+stdenv.mkDerivation rec {
+  pname = "yacd";
+  version = "0.2.14";
 
-  # Use `nix-prefetch-url --unpack https://github.com/haishanh/yacd/archive/{version}.tar.gz` to update
-  src = fetchFromGitHub {
-    owner = "haishanh";
-    repo = "yacd";
-    rev = "v0.2.12";
-    sha256 = "sha256-n0gMW+DWUD/XDJHCdt5LyQQZhWT9E8TlgO53+mky4Xk=";
+  src = fetchzip {
+    url = "https://github.com/haishanh/yacd/releases/download/v${version}/yacd.tar.xz";
+    sha256 = "sha256-q5U3XwYb9gwpxPzqhXC8UkDTSUY5291AOZDexNRMFHM=";
   };
 
-  packageJSON = "${src}/package.json";
-  yarnLock = "${src}/yarn.lock";
-
-  # Since mkYarnPackage moves content of . to ./deps/${pname}, original path should be patched.
-  prePatch = ''
-    substituteInPlace ./src/components/Root.css \
-      --replace '../../node_modules' '../../../../node_modules'
-  '';
-
-  buildPhase = ''
-    yarn build
-  '';
-
-  # installPhase should not be overwritten since it is useful for distPhase.
-  postInstall = ''
-    cp -r $out/libexec/yacd/deps/yacd/public/* $out/bin/
+  installPhase = ''
+    mkdir -p $out/bin
+    cp -r . $out/bin
   '';
 
   meta = with lib; {
     description = "Yet Another Clash Dashboard";
     homepage = "https://github.com/haishanh/yacd";
     license = licenses.free;
-    platforms = [ "x86_64-linux" ];
+    platforms = platforms.all;
   };
 }
